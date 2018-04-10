83a515d79254913bead970f9e08573d94b5e2ede
13111107 <525472231@qq.com>
2018-04-10 22:58:24 +0800

    111

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import domin.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Administrator on 2018/4/7.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        List<Student> arrayList = new ArrayList<Student>();
+        List<Student> outputList = new ArrayList<Student>();
+  
+
+    }
+}
